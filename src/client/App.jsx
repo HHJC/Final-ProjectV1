7be84f964d50e6bd391e557caf8ddb4723125d7e
Hallie Damber <hallie@Hallies-MacBook-Pro.local>
2020-04-10 09:38:21 -0500

    hallie

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,16 +7,9 @@ import Recipes from "./components/Recipes";
 import StoryBooks from "./Views/StoryBookView/StoryBooks";
 import RecipeVideos from "./Views/RecipeVideos";
 import Space from './Views/Space/Space';
-<<<<<<< HEAD
-import Words from "./components/Words";
-import Animals from "./components/Animals";
-//import AnimalCard from './Views/Animalsview/AnimalCard';
-//import WordCard from './Views/Words/WordCard';
-=======
 import AnimalCard from './Views/Animalsview/AnimalCard';
 import WordCard from './Views/Words/WordCard';
 import FlashCard from './Views/ABCview/FlashCard'
->>>>>>> 9b5bcf8cd8e54053a7eb9e183c68eb908dc4ba8c
 
 
 
