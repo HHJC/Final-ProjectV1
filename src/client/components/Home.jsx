429f4f57ea3b506737a998dcd912bc2e32bb11e7
cahsegraffeo <chasegraffeo@gmail.com>
2020-04-14 16:39:01 -0500

    adding cards

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -67,6 +67,17 @@ import { FaBookReader } from 'react-icons/fa';
 </Card>
 
 <Card className="m-3 mt-5 shadow border" style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://cdn6.aptoide.com/imgs/b/9/9/b9999de5dfddb50276a4b16cb83723c4_icon.png" style={{ height: '14rem' }} />
+  <Card.Body>
+    <Card.Title>Math Fun</Card.Title>
+    <Card.Text>
+      Basic Adding & Subtracting.
+    </Card.Text>
+    <Button onClick={()=>history.push('./Views/Mathview/Math')}  className="btn btn-danger m-auto d-flex"><GiCardRandom className="m-1 d-flex align-bottom"/>Solve Problems</Button>
+  </Card.Body>
+</Card>
+
+<Card className="m-3 mt-5 shadow border" style={{ width: '18rem' }}>
   <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/illustration-story-book-cartoon-princesses-princes-front-castle-story-book-cartoon-princesses-princes-129245747.jpg" style={{ height: '14rem' }} />
   <Card.Body>
     <Card.Title>Story Books</Card.Title>
