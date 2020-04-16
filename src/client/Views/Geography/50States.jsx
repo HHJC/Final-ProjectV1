13337c0980fc459305a1549e5cb8b9bf4ceb99f2
Hallie Damber <hallie@Hallies-MacBook-Pro.local>
2020-04-16 17:16:03 -0500

    states geography

--- src/client/Views/Geography/50States.jsx
+++ src/client/Views/Geography/50States.jsx
@@ -15,11 +15,11 @@ const States = () =>  {
   
     <Container className="main row d-flex justify-content-center m-auto p-auto" style={{width:'100rem'}}>
    
-<Card className="m-3 mt-5 shadow border align-items-center" style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="https://images.vexels.com/media/users/3/147813/isolated/preview/5988bedc8ec7b42d0d999d3c0dc350cd-illionois-state-plain-map-by-vexels.png" />
+<Card className="m-3 mt-5 shadow border align-items-center" style={{ width: '50rem' }}>
+  <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Map_of_USA_with_state_names.svg/959px-Map_of_USA_with_state_names.svg.png" />
   <Card.Body>
     <Card.Title>Here is a map of the United States of America!</Card.Title>
-    <Button onClick={()=>history.push('/Views/Geography/StateCards')}  className="btn btn-danger m-auto d-flex"><FaMapMarkerAlt className="m-1 d-flex align-bottom"/> Click me</Button>
+    <Button onClick={()=>history.push('/Views/Geography/StateCards')}  className="btn btn-danger m-auto d-flex"><FaMapMarkerAlt className="m-1 d-flex align-bottom"/> Click me to learn about each state!</Button>
   </Card.Body>
 </Card>
 
