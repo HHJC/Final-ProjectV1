ff732b1d49739b16c617de25f420d1c208b0ee29
cahsegraffeo <chasegraffeo@gmail.com>
2020-04-16 13:13:35 -0500

    pull

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { useHistory } from "react-router-dom";
 import  Navbar  from "react-bootstrap/Navbar";
 import  Nav  from "react-bootstrap/Nav";
 import  Button  from "react-bootstrap/Button";
-//import  {FaUserAlt}  from "react-icons/fa";
+import  {FaUserAlt}  from "react-icons/fa";
 import  {FaHome}  from "react-icons/fa";
 import  {GiCupcake}  from "react-icons/gi";
 // import { Form } from "react-bootstrap";
@@ -20,7 +20,7 @@ const NavBar = () => {
         <Navbar.Brand href="#home">Learn & Play</Navbar.Brand>
         <Nav className="mr-auto ">
         <Button onClick={() => history.push("/")} className="btn-lg btn-warning mx-1 "><FaHome/></Button>
-       {/*  <Button onClick={() => history.push("/User")} className="btn-lg btn-warning mx-1 "><FaUserAlt/></Button> */}
+        <Button onClick={() => history.goBack()} className="btn-lg btn-warning mx-1 "><FaUserAlt/></Button>
         <Button onClick={() => history.push("/Recipes")} className="btn-lg btn-warning mx-1 "><GiCupcake/></Button>
         </Nav>
         </Navbar>
