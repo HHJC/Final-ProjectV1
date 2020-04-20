f8a05201b5e0964afdbad03ec8fcf4f737270bc3
jillianmalea <jillianturman@gmail.com>
2020-04-20 14:44:37 -0700

    commit

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -4,7 +4,7 @@ import  Navbar  from "react-bootstrap/Navbar";
 import  Nav  from "react-bootstrap/Nav";
 import  Button  from "react-bootstrap/Button";
 import  {FaHome}  from "react-icons/fa";
-import  {GiCupcake}  from "react-icons/gi";
+import  {GiShinyApple}  from "react-icons/gi";
 import { IoMdArrowRoundBack} from "react-icons/io";
 // import { Form } from "react-bootstrap";
 // import { FormControl } from "react-bootstrap";
@@ -20,7 +20,7 @@ const NavBar = () => {
         <Navbar.Brand href="#home">Learn & Play</Navbar.Brand>
         <Nav className="mr-auto ">
         <Button onClick={() => history.push("/")} className="btn-md btn-warning mx-1 "><FaHome className="m-1 d-flex justify-content-center m-auto"/> Home</Button>
-        <Button onClick={() => history.push("/Recipes")} className="btn-md btn-warning mx-1 "><GiCupcake className="m-1 d-flex justify-content-center m-auto"/> Recipes</Button>
+        <Button onClick={() => history.push("/About")} className="btn-md btn-warning mx-1 "><GiShinyApple className="m-1 d-flex justify-content-center m-auto"/> About</Button>
         <Button onClick={() => history.goBack()} className="btn-md btn-warning mx-1 "><IoMdArrowRoundBack className="m-1 d-flex justify-content-center m-auto"/> Go Back</Button>
         </Nav>
         </Navbar>
